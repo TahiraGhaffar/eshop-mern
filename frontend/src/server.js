54aeebac14b25f7375c43429d1da4b3b3a4eb542
TahiraGhaffar <tahiraghaffar21@gmail.com>
2023-05-16 21:19:22 +0500

    deployed

---
+++ frontend/src/server.js
@@ -0,0 +1,7 @@
+//export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
+
+export const server = "http://localhost:8001/api/v2";
+
+//export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
+
+export const backend_url = "http://localhost:8001/";
