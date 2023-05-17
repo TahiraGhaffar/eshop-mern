3b0437cd123a689084e3b8f68924fb24edfe9719
TahiraGhaffar <tahiraghaffar21@gmail.com>
2023-05-17 11:32:38 +0500

    backend url done

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,8 +1,9 @@
 //export const server = "https://ecommerce-market-project.herokuapp.com/api/v2";
 
-export const server = "http://localhost:8001/api/v2";
-//export const server = "https://eshop-/api/v2";
+//export const server = "http://localhost:8001/api/v2";
+export const server = "https://eshop-mern-sooty.vercel.app/api/v2";
 
 //export const backend_url = "https://ecommerce-market-project.herokuapp.com/";
 
-export const backend_url = "http://localhost:8001/";
+//export const backend_url = "http://localhost:8001/";
+export const backend_url = "https://eshop-mern-sooty.vercel.app/";
